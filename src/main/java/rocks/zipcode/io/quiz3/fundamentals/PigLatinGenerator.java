db01546691dc851cd67eba95bd9dc1a58c196f31
vle <obentoka@gmail.com>
2020-03-09 12:22:49 -0400

    piglatin done

--- src/main/java/rocks/zipcode/io/quiz3/fundamentals/PigLatinGenerator.java
+++ src/main/java/rocks/zipcode/io/quiz3/fundamentals/PigLatinGenerator.java
@@ -6,14 +6,38 @@ package rocks.zipcode.io.quiz3.fundamentals;
 public class PigLatinGenerator {
 
     public String translate(String str) {
-//        StringBuilder retString = new StringBuilder();
-//        Integer vowelIndex = 0;
-//        if(vowels.contains("" + str.toLowerCase().charAt(0)))
-//            return retString.append("way").toString();
-//        else {
-//            retString.append(str, 1, str.length()).append(str.charAt(0)).append("ay");
-//            return retString.toString();
-//        }
-        return null;
+        StringBuilder retString = new StringBuilder();
+        String[] arrayString = str.split(" ");
+        if(arrayString.length > 1) {
+            for (String i : arrayString) {
+                if (VowelUtils.hasVowels(i)) {
+                    if (!VowelUtils.isVowel(i.charAt(0))) {
+                        retString.append(i, VowelUtils.getIndexOfFirstVowel(i), i.length());
+                        retString.append(i, 0, VowelUtils.getIndexOfFirstVowel(i));
+                        retString.append("ay ");
+                    } else {
+                        retString.append(i).append("way ");
+                    }
+                } else {
+                    retString.append(i).append("ay ");
+                }
+            }
+            retString.replace(retString.lastIndexOf(" "), retString.lastIndexOf(" ")+1, "");
+        }else {
+            for (String i : arrayString) {
+                if (VowelUtils.hasVowels(i)) {
+                    if (!VowelUtils.isVowel(i.charAt(0))) {
+                        retString.append(i, VowelUtils.getIndexOfFirstVowel(i), i.length());
+                        retString.append(i, 0, VowelUtils.getIndexOfFirstVowel(i));
+                        retString.append("ay");
+                    } else {
+                        retString.append(i).append("way");
+                    }
+                } else {
+                    retString.append(i).append("ay");
+                }
+            }
+        }
+        return retString.toString();
     }
 }

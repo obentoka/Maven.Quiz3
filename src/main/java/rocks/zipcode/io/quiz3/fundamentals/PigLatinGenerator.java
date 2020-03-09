f20daaa105ba2090987515c4c832abac44e3ceb9
vle <obentoka@gmail.com>
2020-03-09 10:33:21 -0400

    vowels finished

--- src/main/java/rocks/zipcode/io/quiz3/fundamentals/PigLatinGenerator.java
+++ src/main/java/rocks/zipcode/io/quiz3/fundamentals/PigLatinGenerator.java
@@ -4,7 +4,16 @@ package rocks.zipcode.io.quiz3.fundamentals;
  * @author leon on 09/12/2018.
  */
 public class PigLatinGenerator {
+
     public String translate(String str) {
+//        StringBuilder retString = new StringBuilder();
+//        Integer vowelIndex = 0;
+//        if(vowels.contains("" + str.toLowerCase().charAt(0)))
+//            return retString.append("way").toString();
+//        else {
+//            retString.append(str, 1, str.length()).append(str.charAt(0)).append("ay");
+//            return retString.toString();
+//        }
         return null;
     }
 }
--- src/main/java/rocks/zipcode/io/quiz3/fundamentals/VowelUtils.java
+++ src/main/java/rocks/zipcode/io/quiz3/fundamentals/VowelUtils.java
@@ -4,20 +4,35 @@ package rocks.zipcode.io.quiz3.fundamentals;
  * @author leon on 09/12/2018.
  */
 public class VowelUtils {
+
+    private static String vowels = "aeiou";
+
     public static Boolean hasVowels(String word) {
-        return null;
+        for (int i = 0; i < word.length(); i++) {
+            if(vowels.contains("" + word.toLowerCase().charAt(i)))
+                return true;
+        }
+        return false;
     }
 
     public static Integer getIndexOfFirstVowel(String word) {
-        return null;
+        for (int i = 0; i < word.length(); i++) {
+            if(isVowel(word.charAt(i)))
+                return i;
+        }
+        return -1;
     }
 
 
     public static Boolean startsWithVowel(String word) {
-        return null;
+        if(getIndexOfFirstVowel(word) == 0)
+            return true;
+        return false;
     }
 
     public static Boolean isVowel(Character character) {
-        return null;
+        if(vowels.contains(character.toString().toLowerCase()))
+            return true;
+        return false;
     }
 }

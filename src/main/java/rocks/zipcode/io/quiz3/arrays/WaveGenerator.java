949f28cd9206479b4e52da89a96bb61c9be391a9
vle <obentoka@gmail.com>
2020-03-09 12:34:47 -0400

    wave done

--- src/main/java/rocks/zipcode/io/quiz3/arrays/WaveGenerator.java
+++ src/main/java/rocks/zipcode/io/quiz3/arrays/WaveGenerator.java
@@ -1,10 +1,30 @@
 package rocks.zipcode.io.quiz3.arrays;
 
+import rocks.zipcode.io.quiz3.fundamentals.StringUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author leon on 09/12/2018.
  */
 public class WaveGenerator {
+
+    static String alpha = "abcdefghijklmnopqrstuvwxyz";
+
     public static String[] wave(String str) {
-        return null;
+        String low = str.toLowerCase();
+        List<String> stringArray = new ArrayList<>();
+        for (int i = 0; i < low.length(); i++) {
+            if(alpha.contains("" + low .charAt(i))){
+                String waveString = StringUtils.capitalizeNthCharacter(low, i);
+                stringArray.add(waveString);
+            }
+        }
+        String[] retArray = new String[stringArray.size()];
+        for (int i = 0; i < stringArray.size(); i++) {
+            retArray[i] = stringArray.get(i);
+        }
+        return retArray;
     }
 }
--- src/main/java/rocks/zipcode/io/quiz3/objectorientation/enums/RockPaperScissorHandSign.java
+++ src/main/java/rocks/zipcode/io/quiz3/objectorientation/enums/RockPaperScissorHandSign.java
@@ -4,7 +4,9 @@ package rocks.zipcode.io.quiz3.objectorientation.enums;
  * @author leon on 09/12/2018.
  */
 public enum RockPaperScissorHandSign {
-    ADD_ENUMERATIONS_HERE;
+    SCISSOR_HAND_SIGN,
+    ROCK_PAPER_SCISSOR_HAND_SIGN,
+    PAPER_SCISSOR_HAND_SIGN;
 
     public RockPaperScissorHandSign getWinner() {
         return null;
